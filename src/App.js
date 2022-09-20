8e74709cf9ffe4bdd46a2014dbb3b93f0dc939e2
Andrii Yelnikov <whiteyelnikov@gmail.com>
2022-09-20 22:29:30 +0300

    first solution

--- src/App.js
+++ src/App.js
@@ -1,56 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import './App.scss';
+import { useState, useEffect } from 'react';
+import { Navigation } from './Navigation/Navigation';
+import { Main } from './Main/Main';
+import { getData } from "./api";
+import { useSelector } from 'react-redux';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+  const [appliedInput, setAppliedInput] = useState('');
+  const appliedRegions = useSelector(state => state.store.appliedOptionsRegion);
+  const appliedStatuses = useSelector(state => state.store.appliedOptionsStatus);
+  const appliedRatings = useSelector(state => state.store.appliedOptionsRating);
+
+  const setQuery = (input) => {
+    setAppliedInput(input);
+  }
+
+  useEffect(() => {
+    getData()
+      .then(res => {
+        if (res !== undefined) {
+          let filtered = [];
+
+          if (appliedInput !== '') {
+            filtered = res.filter(obj => obj.name.toLowerCase().includes(appliedInput.toLowerCase() || obj.id.toLowerCase().includes(appliedInput.toLowerCase()) || obj.lead.toLowerCase().includes(appliedInput.toLowerCase())));
+          } else {
+            filtered = [...res];
+          }
+          let selected = [...filtered];
+          if (appliedRegions.length > 0) {
+            selected = filtered.filter(el => appliedRegions.includes(el.region))
+          }
+
+          if (appliedRatings.length > 0) {
+            selected = selected.filter(el => appliedRatings.includes(el.rating))
+          }
+
+          if (appliedStatuses.length > 0) {
+            selected = selected.filter(el => appliedStatuses.includes(el.status))
+          }
+
+          setData(selected);
+        }
+      })
+  }, [appliedInput, appliedRegions, appliedStatuses, appliedRatings])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <Navigation setQuery={setQuery} />
+      <Main data={data} />
     </div>
   );
 }
